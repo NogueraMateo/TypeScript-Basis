473885b3ac8504b68cff0670b8c6f36f5d870934
NogueraMateo <mateo.edu.co@gmail.com>
2024-09-15 12:41:03 -0500

    Objects covered

--- dist/objetos.js
+++ dist/objetos.js
@@ -13,8 +13,12 @@ let myHero = {
     power: 6000
 };
 function createMyHero(name, age, power) {
-    return { name, age, power, isActive: true };
+    return { id: crypto.randomUUID(), name, age, power, isActive: true };
 }
 const deadpool = createMyHero("Deadpool", 40, 2000);
 console.log(deadpool.isActive);
+const userAddress = {
+    street1: "Street 21",
+    street2: "45Nth"
+};
 //# sourceMappingURL=objetos.js.map
