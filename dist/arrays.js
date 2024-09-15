6e93c051fccb1d0e3020b338a40da3ca8149686b
NogueraMateo <mateo.edu.co@gmail.com>
2024-09-15 12:40:16 -0500

    Arrays covered

---
+++ dist/arrays.js
@@ -0,0 +1,9 @@
+"use strict";
+const languages = [];
+languages.push("TypeScript");
+const game = [
+    ["O", "X", ""],
+    ["O", "X", ""],
+    ["O", "X", ""]
+];
+//# sourceMappingURL=arrays.js.map
